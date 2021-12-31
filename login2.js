bbdaa7c63fa595d7049f4860ac9440c28746c9d4
leuenbergern <84071498+leuenbergern@users.noreply.github.com>
2021-12-31 10:17:17 +0100

    Add files via upload

---
+++ login2.js
@@ -0,0 +1,16 @@
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username == "admin" && password == "1234") {
+        alert("Login erfolgreich");
+        window.open("works.html");
+    } else {
+        alert("Login fehlgeschlagen");
+    }
+
+
+}
+
+
+

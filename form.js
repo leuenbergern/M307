29fc6883982dbe5027c76307ac62d2649767aafe
leuenbergern <84071498+leuenbergern@users.noreply.github.com>
2022-06-16 15:09:15 +0200

    Validation

--- form.js
+++ form.js
@@ -1,16 +1,50 @@
 function validate() {
-    const express = require('express')
-    const app = express()
-    const port = 3000
-    
-    app.get('/', (req, res) => {
-      res.send('Hello World!')
-    })
-    
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
-    })
+  const name = document.getElementById('name');
+  if (name.minlength = "4") {
+    alert("Bitte geben sie mindestens 4 Zeichen ein")
+  }
 }
+
+
+
+
+function validate() {
+  const telefon = document.getElementById('telefon');
+  if (telefon.value !== "123") {
+    alert("Sie haben eine ungültige Telefonnummer eingegeben")
+  }
+  
+}
+
+
+
+function validate() {
+  const email = document.getElementById('email');
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))
+  {
+    return (true)
+  }
+    alert("Sie haben eine ungültige Email-Adresse eingegeben")
+    return (false)
+  }
+
+
+  function validate() {
+    const informationen = document.getElementById('informationen');
+    if (informationen.maxlength = "100") {
+      alert("Bitte geben sie maximal 100 Zeichen ein")
+    }
+  }
+
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(e) {
+  //https://www.w3schools.com/jsref/event_preventdefault.asp
+  e.preventDefault();
+  //First validate form
+  validate();
+});
 
 
 
--- validation_server/app.js
+++ validation_server/app.js
@@ -1,9 +1,18 @@
 const express = require('express')
-const app = express()
+let bodyParser = require("body-parser");
+const app = express() 
+app.use(bodyParser.json());
+// support encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/formular', (req, res) => {
+  console.log (req.body)
+  res.send('Hallo ' + req.body.telefon)
 })
 
 app.listen(port, () => {
